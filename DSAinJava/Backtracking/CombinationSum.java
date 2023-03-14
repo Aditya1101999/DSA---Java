ace39056ecc9b964a4fcb0796326cc7b3f628b76
adi <adi854105@gmail.com>
2023-03-14 16:39:09 +0530

    .

--- DSAinJava/Backtracking/CombinationSum.java
+++ DSAinJava/Backtracking/CombinationSum.java
@@ -20,4 +20,24 @@ public class CombinationSum {
         }
         findCombinations(index+1,candidates,target,ans,curr);
     }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {//no duplicates
+        List<List<Integer>>ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombination2(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+    private void findCombination2(int index,int[] candidates,int target,List<List<Integer>>ans,ArrayList<Integer>curr){
+        if(target==0){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i=index;i<candidates.length;i++){
+            if(i>index && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+
+            curr.add(candidates[i]);
+            findCombination2(i+1,candidates,target-candidates[i],ans,curr);
+            curr.remove(curr.size()-1);
+        }
+    }
 }
