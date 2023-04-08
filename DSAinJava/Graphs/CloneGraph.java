afd9fbcb747578f595c9f376716dfa726c2bb679
adi <adi854105@gmail.com>
2023-04-08 17:59:21 +0530

    .

---
+++ DSAinJava/Graphs/CloneGraph.java
@@ -0,0 +1,62 @@
+package DSAinJava.Graphs;
+import java.util.*;
+public class CloneGraph {
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    Map<Node,Node>map=new HashMap<>();
+    public Node cloneGraphDFS(Node node) {
+        if(node==null){
+            return null;
+        }
+        if(map.containsKey(node)){
+            return map.get(node);
+        }
+        Node copy=new Node(node.val);
+        map.put(node,copy);
+
+        List<Node>neighbors=new ArrayList<>();
+        for(Node neighbor :node.neighbors){
+            Node copiedNeighbor=cloneGraphDFS(neighbor);
+            neighbors.add(copiedNeighbor);
+        }
+        copy.neighbors=neighbors;
+        return copy;
+
+    }
+    public Node cloneGraphBFS(Node node) {
+        if(node==null){
+            return null;
+        }
+        Map<Node,Node>map=new HashMap<>();
+        Queue<Node>q=new LinkedList<>();
+        Node copy=new Node(node.val);
+        q.add(node);
+        map.put(node,copy);
+
+        while(!q.isEmpty()){
+            Node curr=q.remove();
+            for(Node neighbor : curr.neighbors){
+                if(!map.containsKey(neighbor)){
+                    map.put(neighbor,new Node(neighbor.val));
+                    q.add(neighbor);
+                }
+                map.get(curr).neighbors.add(map.get(neighbor));//copy->copy of neighbor
+            }
+        }
+        return copy;
+    }
+}
