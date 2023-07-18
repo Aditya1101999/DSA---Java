ce24971e12b206009dda0b509334efc46569c897
Aditya Choudhary <adi854105@gmail.com>
2023-07-18 22:41:35 +0530

    .

--- DSAinJava/Graphs/SumofDistances.java
+++ DSAinJava/Graphs/SumofDistances.java
@@ -26,11 +26,13 @@ public class SumofDistances {
             count[root]+=count[i];
             res[root]+=res[i]+count[i];
         }
-        count[root]++;
+        count[root]++;//adding one node as the root itself
     }
     public static void preOrder(int root,int prev){
         for(int i:tree.get(root)){//all the children of given root
             if(i==prev) continue;
+            //count[i] is the node count which are coming closer , so their distance will be reduced
+            //similarly , n-count[i] are the nodes going farther , so distance will increase
             res[i]=res[root]-count[i]+(count.length-count[i]);
             preOrder(i,root);
         }
